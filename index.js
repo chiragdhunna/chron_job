cb1f922e82e85eb95755dafb5c77d214115586e0
chiragdhunna <chiragdhunna2468@gmail.com>
2024-11-28 20:31:15 +0530

    looping chron job to itself

--- index.js
+++ index.js
@@ -7,11 +7,8 @@ app = express();
 // Creating a cron job which runs on every 10 second
 cron.schedule("*/10 * * * * *", function () {
   console.log("running a task every 10 second");
-  const response = fetch("https://chat-go-taupe.vercel.app/").then(
-    (res, rej) => {
-      console.log(`res : ${res.data}`);
-    }
-  );
+  fetch("https://chat-go-taupe.vercel.app/").then((res, rej) => {});
+  fetch("https://chron-job.onrender.com/").then((res, rej) => {});
 });
 
 app.listen(3000);
